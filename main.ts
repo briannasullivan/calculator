e3ff36652d1f2c1186a7ad58f40e198509a63b04
briannasullivan <113055705+briannasullivan@users.noreply.github.com>
2022-09-16 14:40:31 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+input.onButtonPressed(Button.A, function () {
+    A = num + 9
+    basic.showNumber(A)
+    B = num - 9
+    basic.showNumber(B)
+    C = num * 2
+    basic.showNumber(C)
+    D = num / 3
+    basic.showNumber(D)
+    E = num / 4
+    basic.showNumber(E)
+    F = Math.sqrt(0)
+    basic.showNumber(F)
+    G = Math.round(0.75)
+    basic.showNumber(G)
+    H = num - 9
+    basic.showNumber(H)
+    I = num - 9
+    basic.showNumber(I)
+    J = num - 9
+    basic.showNumber(J)
+})
+let J = 0
+let I = 0
+let H = 0
+let G = 0
+let F = 0
+let E = 0
+let D = 0
+let C = 0
+let B = 0
+let A = 0
+let num = 0
+basic.showIcon(IconNames.Heart)
+num = 0
+basic.forever(function () {
+	
+})
